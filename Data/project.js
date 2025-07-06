38f79af4f421fe2abbec68209be0a5099525ad47
FavourPeters <favourpeters1@gmail.com>
2025-07-06 20:02:47 +0100

    New Project alert. Media Content bank

--- Data/project.js
+++ Data/project.js
@@ -1,16 +1,36 @@
 const projects = [
   {
     id: 1,
-    title: "Digireal - Real Estate",
+    title: "Media Content Bank",
     description:
-      "A real estate platform with user authentication and property listings.",
-    image: "/digireal.png",
-    link: "https://www.digireal.net/",
-    github: "https://github.com/yourusername/bondtest",
+      "A media content bank users to draw inspiration from various media content.",
+    image: "/mcb.png",
+    link: "https://media-content-bank.netlify.app/",
+    github: "private",
     techStack: ["Nextjs", "Nodejs", "Express.js", "MongoDB"],
   },
   {
     id: 2,
+    title: "Digireal - Real Estate",
+    description:
+      "A real estate platform featuring user authentication and property listings. Backend developed by me.",
+    image: "/digireal.png",
+    link: "https://www.digireal.net/",
+    github: "private",
+    techStack: ["Nextjs", "Nodejs", "Express.js", "MongoDB"],
+  },
+  {
+    id: 3,
+    title: "Hospiyou",
+    description:
+      "A landing page for a healthcare platform with email subscription.",
+    image: "/hospiyou.png",
+    link: "https://hospiyou.com/",
+    github: "private",
+    techStack: ["React", "Express.js", "MongoDB"],
+  },
+  {
+    id: 4,
     title: "Rock Paper Scissors",
     description:
       "An exciting and popular game. This is one of my practice projects with the use of javaScript OOP.",
@@ -18,16 +38,6 @@ const projects = [
     link: "https://rocks-papers-scissorrs.netlify.app/",
     github: "https://github.com/FavourPeters/rock-paper_scissors",
     techStack: ["React", "OOP"],
-  },
-  {
-    id: 3,
-    title: "Hospiyou Landing Page",
-    description:
-      "A landing page for a healthcare platform with email subscription.",
-    image: "/hospiyou.png",
-    link: "https://hospiyou.com/",
-    github: "https://github.com/yourusername/bondtest",
-    techStack: ["React", "Express.js", "MongoDB"],
   },
 ];
 
